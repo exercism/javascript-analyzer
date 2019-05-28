a00c0f870aa9cb3163807df6adcd8035693de79a
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-05-28 22:18:14 +0200

    Add explicit return types

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,6 +1,12 @@
 import fs from 'fs'
 
-export async function readFile(filePath: string) {
+/**
+ * Reads a file from a path and resolves when it's done
+ *
+ * @param {string} filePath the path
+ * @returns the promise
+ */
+export async function readFile(filePath: string): Promise<Buffer> {
   return new Promise<Buffer>((resolve, reject) => {
     fs.readFile(filePath, (err, data) => {
       err ? reject(err) : resolve(data)
@@ -8,7 +14,7 @@ export async function readFile(filePath: string) {
   })
 }
 
-export async function readDir(dirPath: string) {
+export async function readDir(dirPath: string): Promise<string[]> {
   return new Promise<string[]>((resolve, reject) => {
     fs.readdir(dirPath, (err, files) => {
       if (err) {
@@ -20,7 +26,7 @@ export async function readDir(dirPath: string) {
   })
 }
 
-export async function exists(path: string) {
+export async function exists(path: string): Promise<boolean> {
   return new Promise<boolean>((resolve) => {
     fs.exists(path, (exists) => {
       resolve(exists)
