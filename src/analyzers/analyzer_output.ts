ebdac6782226a087866d828bc310448e37144b19
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-05-28 22:17:52 +0200

    Add commentary

--- src/analyzers/analyzer_output.ts
+++ src/analyzers/analyzer_output.ts
@@ -3,40 +3,78 @@ import fs from 'fs'
 import { Comment } from '../comments/comment'
 import { ExecutionOptions } from '../utils/execution_options';
 
-export type SolutionStatus =
-    'refer_to_mentor'
-  | 'approve_as_optimal'
-  | 'approve_with_comment'
-  | 'disapprove_with_comment'
+enum SolutionStatus {
+  /** This is the default situation and should be used when there is any uncertainty. */
+  Redirect = 'refer_to_mentor',
+  /** To be used when a solution matches pre-known optimal solutions */
+  ApproveAsOptimal = 'approve_as_optimal',
+  /** To be used when a solution can be approved but with a known improvement. */
+  ApproveWithComment = 'approve_with_comment',
+  /** To be used when a solution can be disapproved as suboptimal and a comment is provided. */
+  DisapproveWithComment = 'disapprove_with_comment'
+}
 
+/**
+ * The interface for the analyzer output is described [here][doc].
+ *
+ * [doc]: https://github.com/exercism/automated-mentoring-support/blob/master/docs/interface.md#output-format
+ *
+ * @export
+ * @class AnalyzerOutput
+ */
 export class AnalyzerOutput {
   public status: SolutionStatus
   public comments: Comment[]
 
   constructor() {
-    this.status = 'refer_to_mentor'
+    this.status = SolutionStatus.Redirect
     this.comments = []
   }
 
+  /**
+   * Mark the solution as approved
+   */
   public approve() {
     this.status = this.comments.length === 0
-      ? 'approve_as_optimal'
-      : 'approve_with_comment'
+      ? SolutionStatus.ApproveAsOptimal
+      : SolutionStatus.ApproveWithComment
   }
 
+  /**
+   * Mark the solution as dissapproved
+   */
   public disapprove() {
-    this.status = 'disapprove_with_comment'
+    this.status = SolutionStatus.DisapproveWithComment
   }
 
+  /**
+   * Mark the solution as refer to mentor
+   */
   public redirect() {
-    this.status = 'refer_to_mentor'
+    this.status = SolutionStatus.Redirect
   }
 
+  /**
+   * Add a comment
+   *
+   * @param {Comment} comment the comment to add
+   * @returns self
+   */
   public add(comment: Comment) {
     this.comments.push(comment)
     return this
   }
 
+  /**
+   * Transforms the output into a structured data string for output.
+   *
+   * The format should match [the output format][doc].
+   *
+   * [doc]: https://github.com/exercism/automated-mentoring-support/blob/master/docs/interface.md#output-format
+   *
+   * @param {ExecutionOptions} options
+   * @returns {string}
+   */
   public toString(options: ExecutionOptions): string {
     return JSON.stringify({
       status: this.status,
@@ -53,6 +91,13 @@ export class AnalyzerOutput {
     }, null, 2)
   }
 
+  /**
+   * Writes self to `path`
+   *
+   * @param {string} path
+   * @param {ExecutionOptions} options
+   * @returns {Promise<void>} The future which resolves / rejects when its done
+   */
   public writeTo(path: string, options: ExecutionOptions): Promise<void> {
     return new Promise((resolve, reject) => {
       fs.writeFile(path, this.toString(options), (err) => {
--- src/comments/comment.ts
+++ src/comments/comment.ts
@@ -1,5 +1,4 @@
 type TemplateKeys = Array<number | string>
-type TemplatedValues = { [key: string]: string | undefined }
 
 export type FactoryResultParameter<R extends string = string> = Array<string> | [Record<R, string | undefined>]
 
@@ -8,7 +7,7 @@ export class Comment {
   constructor(
     public readonly message: string,
     public readonly template: string,
-    public readonly variables: { [name: string]: string | undefined, [name: number]: string | undefined },
+    public readonly variables: Readonly<{ [name: string]: string | undefined, [name: number]: string | undefined }>,
     public readonly externalTemplate: string
   ) {}
 
@@ -19,6 +18,35 @@ export class Comment {
 
 export type CommentFactory<R extends string> = (...values: FactoryResultParameter<R>) => Comment
 
+/**
+ * Creates a comment factory that can be used to generate a comment.
+ *
+ * @example
+ *
+ * const NO_PARAMETER = factory<'function_name'>`
+ * Your function \`${'function_name'}\` does not have a parameter.
+ * The tests won't pass without it.
+ * `('javascript.generic.no_parameter')
+ *
+ * NO_PARAMETER({ function_name: 'foo' })
+ * //
+ * // Generates a comment object that holds enough information to be output
+ * // correctly, regardless of the current ExecutionOptions.
+ * //
+ * // => Comment({
+ * //      message: "Your function \`foo\` does not have a parameter.\nThe tests won't pass without it.",
+ * //      template: "Your function \`{function_name}\` does not have a parameter",
+ * //      variables: {
+ * //        function_name: "foo"
+ * //      },
+ * //      external_template: "javascript.generic.no_parameter"
+ * //    })
+ *
+ * @template R A list of keys 'key' | 'another_key' that are necessary
+ * @param {TemplateStringsArray} strings automatically assigned
+ * @param {...TemplateKeys} keys automatically assigned
+ * @returns the factory that can generate Comments
+ */
 export function factory<R extends string = ''>(strings: TemplateStringsArray, ...keys: TemplateKeys) {
   return (externalTemplate: string): CommentFactory<R> => {
     return (function(...values: FactoryResultParameter<R>): Comment {
