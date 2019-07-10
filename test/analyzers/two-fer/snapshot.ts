e5934c36cb4821820a8bf8f80c83812488d4265c
crash <snickerless1@shaw.ca>
2019-07-09 19:34:27 -0600

    updated tests

--- test/analyzers/two-fer/snapshot.ts
+++ test/analyzers/two-fer/snapshot.ts
@@ -8,14 +8,15 @@ const snapshotTestsGenerator = makeTestGenerator(
 
 describe('When running analysis on two-fer fixtures', () => {
   snapshotTestsGenerator('approve', [
-    118, 129, 139, 313, 400, 493, 70
+    129, 139, 400, 493, 70
   ])
   snapshotTestsGenerator('disapprove', [
     1, 10, 100, 101, 102, 104, 105, 107, 109, 11,
-    110, 111, 112, 114, 115, 116, 12, 121, 123, 124
+    110, 111, 112, 114, 115, 116, 12, 121, 123, 124,
+    118, 120, 313
   ])
   snapshotTestsGenerator('refer_to_mentor', [
-    0, 103, 106, 108, 113, 119, 120, 122, 133, 138,
+    0, 103, 106, 108, 113, 119, 122, 133, 138,
     143, 147, 148, 166, 171, 175, 181, 183, 192, 194
   ])
 })
