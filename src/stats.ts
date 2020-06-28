a51a9f37bae2e26fda76e9f6d3f6f2f376d83d22
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2020-06-28 05:34:05 +0200

    Update stats.ts

--- src/stats.ts
+++ src/stats.ts
@@ -1,12 +1,11 @@
+import { Node } from "@typescript-eslint/typescript-estree/dist/ts-estree/ts-estree";
 
-import { Node } from '@typescript-eslint/typescript-estree/dist/ts-estree/ts-estree';
+import path from "path";
 
-import path from 'path';
-
-import { DirectoryInput } from '~src/input/DirectoryInput';
-import { AstParser } from '~src/parsers/AstParser';
-import { Bootstrap } from '~src/utils/bootstrap';
-import { readDir } from '~src/utils/fs';
+import { DirectoryInput } from "~src/input/DirectoryInput";
+import { AstParser } from "~src/parsers/AstParser";
+import { Bootstrap } from "~src/utils/bootstrap";
+import { readDir } from "~src/utils/fs";
 
 // The bootstrap call uses the arguments passed to the process to figure out
 // which exercise to target, where the input lives (directory input) and what
@@ -18,65 +17,85 @@ import { readDir } from '~src/utils/fs';
 // exercise analyzer (dry: without output) for all the folders inside the
 // two-fer fixture folder, with console log output turned on
 //
-const { exercise, options, logger } = Bootstrap.call()
+const { exercise, options, logger } = Bootstrap.call();
 
-const FIXTURES_ROOT = path.join(options.inputDir || path.join(__dirname, '..', 'test', 'fixtures'), exercise.slug)
+const FIXTURES_ROOT = path.join(
+  options.inputDir || path.join(__dirname, "..", "test", "fixtures"),
+  exercise.slug
+);
 
-function pad(value: string | number, pad = '       '): string {
-  return (pad + value).slice(-pad.length)
+function pad(value: string | number, pad = "       "): string {
+  return (pad + value).slice(-pad.length);
 }
 
-logger.log(`=> start statistics collection for ${exercise.slug}`)
+logger.log(`=> start statistics collection for ${exercise.slug}`);
 
-const parser = new AstParser({ comment: false, tokens: false, loc: false, range: false })
+const parser = new AstParser({
+  comment: false,
+  tokens: false,
+  loc: false,
+  range: false,
+});
 
 readDir(FIXTURES_ROOT)
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  .then(async (fixtureDirs) => Promise.all(fixtureDirs.map(async (fixtureDir) => {
-    const inputDirectory = path.join(FIXTURES_ROOT, fixtureDir)
-    try {
-      const input = new DirectoryInput(inputDirectory, exercise.slug)
-      const results = await parser.parse(input)
+  .then(async (fixtureDirs) =>
+    Promise.all(
+      fixtureDirs.map(async (fixtureDir) => {
+        const inputDirectory = path.join(FIXTURES_ROOT, fixtureDir);
+        try {
+          const input = new DirectoryInput(inputDirectory, exercise.slug);
+          const results = await parser.parse(input);
 
-      if (results.length === 0) {
-        throw new Error(`No input source files for ${exercise.slug}`)
-      }
-
-      const [{ program: root }] = results
-
-      // There can be a bug where loc and range data is not removed
-      if (root.loc || root.range) {
-        delete root.comments
-        delete root.tokens
-        delete root.loc
-        delete root.range
-
-        require('eslint/lib/util/traverser').traverse(
-          root, {
-            // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-            enter(node: Node) {
-              delete node.loc
-              delete node.range
-            },
-
-            // Use typescript visitor keys (otherwise type annotations are not removed)
-            visitorKeys: require("@typescript-eslint/parser/dist/visitor-keys").visitorKeys
+          if (results.length === 0) {
+            throw new Error(`No input source files for ${exercise.slug}`);
           }
-        )
-      }
 
-      return JSON.stringify(root)
-    } catch ({ message, ...other}) {
-      logger.error(`=> skipping ~${path.relative(path.dirname(FIXTURES_ROOT), inputDirectory)}`)
-      logger.error(`   ${message}${Object.keys(other).length > 0 ? ` (${JSON.stringify(other)})` : ''}\n`)
-      return undefined
-    }
-  })))
+          const [{ program: root }] = results;
+
+          // There can be a bug where loc and range data is not removed
+          if (root.loc || root.range) {
+            delete root.comments;
+            delete root.tokens;
+            delete root.loc;
+            delete root.range;
+
+            require("eslint/lib/shared/traverser").traverse(root, {
+              // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+              enter(node: Node) {
+                delete node.loc;
+                delete node.range;
+              },
+
+              // Use typescript visitor keys (otherwise type annotations are not removed)
+              visitorKeys: require("@typescript-eslint/typescript-estree/dist/visitor-keys")
+                .visitorKeys,
+            });
+          }
+
+          return JSON.stringify(root);
+        } catch ({ message, ...other }) {
+          logger.error(
+            `=> skipping ~${path.relative(
+              path.dirname(FIXTURES_ROOT),
+              inputDirectory
+            )}`
+          );
+          logger.error(
+            `   ${message}${
+              Object.keys(other).length > 0 ? ` (${JSON.stringify(other)})` : ""
+            }\n`
+          );
+          return undefined;
+        }
+      })
+    )
+  )
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   .then((trees) => trees.filter(Boolean) as readonly string[])
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   .then((trees) => {
-    const realTrees = trees.filter(Boolean)
+    const realTrees = trees.filter(Boolean);
     const counts = {
       invalid: trees.length - realTrees.length,
       valid: realTrees.length,
@@ -84,15 +103,16 @@ readDir(FIXTURES_ROOT)
       unique: Object.keys(
         // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
         realTrees.reduce((counts, tree) => {
-          counts[tree] = (counts[tree] || 0) + 1
-          return counts
-        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+          counts[tree] = (counts[tree] || 0) + 1;
+          return counts;
+          // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
         }, {} as { [tree: string]: number })
-      ).length
-    }
+      ).length,
+    };
 
-    const { total, unique, valid, invalid } = counts
-    process.stdout.write(`
+    const { total, unique, valid, invalid } = counts;
+    process.stdout.write(
+      `
 ## Raw output
 
 \`\`\`json
@@ -107,5 +127,6 @@ location data (whitespace) and other tokens.
 |   total |  unique |   valid | invalid |
 |--------:|--------:|--------:|--------:|
 | ${pad(total)} | ${pad(unique)} | ${pad(valid)} | ${pad(invalid)} |
-  `.trim())
-  })
+  `.trim()
+    );
+  });
