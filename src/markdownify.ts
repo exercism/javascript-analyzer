c7de1c827f3d6960c39809f2a0a830f9253be6f7
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-01-11 22:19:39 +0100

    Update and format

--- src/markdownify.ts
+++ src/markdownify.ts
@@ -1,10 +1,8 @@
-
-import path from 'path';
-import childProcess from 'child_process';
-
-import { Bootstrap } from '~src/utils/bootstrap'
-import { readFile } from '~src/utils/fs'
-import { spawnSync } from 'child_process';
+import { readFile } from '@exercism/static-analysis'
+import { spawnSync } from 'child_process'
+import path from 'path'
+import { Output } from './interface'
+import { Bootstrap } from './utils/bootstrap'
 
 // The bootstrap call uses the arguments passed to the process to figure out
 // which exercise to target, where the input lives (directory input) and what
@@ -20,26 +18,39 @@ const { options, logger } = Bootstrap.call()
 logger.log('=> DEBUG mode is on')
 
 readFile(path.join(options.inputDir, 'analysis.json'))
-  .then((jsonString: Buffer | string): Pick<Output, 'status' | 'comments'> => JSON.parse(jsonString.toString()))
-  .then((output: Pick<Output, 'status' | 'comments'> ): void => {
-    logger.log(`=> Got ${output.status} with ${output.comments.length} comments`)
-    const spawned = childProcess.spawnSync(
+  .then(
+    (jsonString: Buffer | string): Pick<Output, 'status' | 'comments'> =>
+      JSON.parse(jsonString.toString())
+  )
+  .then((output: Pick<Output, 'status' | 'comments'>): void => {
+    logger.log(
+      `=> Got ${output.status} with ${output.comments.length} comments`
+    )
+    const spawned = spawnSync(
       'ruby',
       [
         'bin/markdownify.rb',
         path.resolve(options.inputDir),
-        path.resolve(path.join(__dirname, '..', '..', 'exercism-website-copy', 'automated-comments'))
+        path.resolve(
+          path.join(
+            __dirname,
+            '..',
+            '..',
+            'exercism-website-copy',
+            'automated-comments'
+          )
+        ),
       ],
       {
         stdio: 'pipe',
         cwd: process.cwd(),
-        env: process.env
+        env: process.env,
       }
     )
 
     spawned.error && logger.log(spawned.error.toString())
     if (spawned.output) {
-      const [,out, err] = spawned.output
+      const [, out, err] = spawned.output
       if (out) {
         logger.log(out.toString().trim())
       }
@@ -50,4 +61,3 @@ readFile(path.join(options.inputDir, 'analysis.json'))
     }
   })
   .catch((err): void => logger.error(err))
-
