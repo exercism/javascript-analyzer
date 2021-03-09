605a2a7ec36efdfe2c9436647f1912b91982558e
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-03-09 03:23:04 +0100

    Bump the version

--- src/comments/shared.ts
+++ src/comments/shared.ts
@@ -128,7 +128,7 @@ Expected source file "${'expected'}", found: ${'available'}.
 export const EXEMPLAR_SOLUTION = factory`
 🎉 That is an exemplar solution. Congratulations. It is exactly what we think
 is the most idiomatic implementation of the tasks at hand. Rejoice!
-`('javascript.generic.exemplar', CommentType.Celebratory)
+`('javascript.general.exemplar', CommentType.Celebratory)
 
 export const FUNCTION_NOT_OPTIMAL = factory<'function'>`
 📕 It looks like ${'function'} is not optimal. This analyzer is a Work In
@@ -136,7 +136,7 @@ Progress and can't tell you exactly why it thinks something is not optimal. Feel
 free to ignore this feedback. If you wish to attempt to resolve it, the advice
 is as follows: this function is expected to be as simple as possible, without
 declaring any extra variables.`(
-  'javascript.generic.function_not_optimal',
+  'javascript.general.function_not_optimal',
   CommentType.Informative
 )
 
@@ -146,7 +146,7 @@ provides the correct exports and correct function declarations, with the
 expected amount and format of parameters. It is sometimes possible to change the
 function signature (change how its parameters work), but in this case the
 parameters were already optimally defined.
-`('javascript.generic.signature_changed', CommentType.Informative)
+`('javascript.general.signature_changed', CommentType.Informative)
 
 export const REPLACE_MAGIC_WITH_IDENTIFIER = factory<'literal' | 'identifier'>`
 The a magic value \`${'literal'}\` can be replaced by \`${'identifier'}\`. When
