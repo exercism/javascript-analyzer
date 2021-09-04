b01970b0f0c70296faa7d572958a65ca2e9d1ae0
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-09-04 18:45:48 +0200

    Debugging information

--- bin/check-formatting.sh
+++ bin/check-formatting.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 if [ -z "$EXERCISM_PRETTIER_VERSION" ]; then
-	echo "Pulling prettier version from package.json"
-	EXERCISM_PRETTIER_VERSION=$(npm list prettier | grep -Po '.*prettier@\K.*')
+  echo "Pulling prettier version from package.json"
+  # EXERCISM_PRETTIER_VERSION=$(npm list prettier | grep -Po '.*prettier@\K.*')
 fi
 
 if [ -z "$EXERCISM_PRETTIER_VERSION" ]; then
-	echo "This script requires the EXERCISM_PRETTIER_VERSION variable to work."
-	echo "Please see https://github.com/exercism/v3/blob/master/docs/maintainers/style-guide.md for guidance."
-	exit 1
+  echo "---------------------------------------------------"
+  echo "This script requires the EXERCISM_PRETTIER_VERSION variable to work."
+  echo "Please see https://exercism.org/docs/building/markdown/style-guide for guidance."
+  echo "---------------------------------------------------"
+  echo "This is what npm reports:"
+  echo "$(npm list prettier)"
+  echo ""
+  echo "This is the version that can be extracted:"
+  echo "$(npm list prettier | grep -Po '.*prettier@\K.*')"
+  echo ""
+  echo "These files are found in the repo root:"
+  echo "$(ls -p | grep -v /)"
+  echo "---------------------------------------------------"
+  exit 1
 else
-	echo "Running format with prettier@$EXERCISM_PRETTIER_VERSION"
+ echo "Running format with prettier@$EXERCISM_PRETTIER_VERSION"
 fi
 
 npx "prettier@$EXERCISM_PRETTIER_VERSION" --check "**/*.{js,jsx,ts,tsx,css,sass,scss,html,json,md,yml}"
