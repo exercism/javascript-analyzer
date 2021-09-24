c647df5f896d24fca2bece58e6acbc3e3c9c0d23
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-09-24 20:42:22 +0200

    Prepare freelancer-rates analyzer

---
+++ src/analyzers/concept/freelancer-rates/FreelancerRatesSolution.ts
@@ -0,0 +1,100 @@
+import {
+  AstParser,
+  ExtractedFunction,
+  extractExports,
+  extractFunctions,
+} from '@exercism/static-analysis'
+import { TSESTree } from '@typescript-eslint/typescript-estree'
+import { readFileSync } from 'fs'
+import path from 'path'
+import { Source } from '~src/analyzers/SourceImpl'
+import { assertPublicApi } from '~src/asserts/assert_public_api'
+import { PublicApi } from '../../PublicApi'
+import { parameterName } from '../../utils/extract_parameter'
+
+export const DAY_RATE = 'dayRate'
+export const MONTH_RATE = 'monthRate'
+export const DAYS_IN_BUDGET = 'daysInBudget'
+
+class DayRate extends PublicApi {
+  public get ratePerHourParameter(): string {
+    return this.parameter
+  }
+}
+
+class MonthRate extends PublicApi {
+  public get ratePerHourParameter(): string {
+    return this.parameter
+  }
+
+  public readonly discountParameter: string
+
+  constructor(implementation: ExtractedFunction) {
+    super(implementation)
+
+    this.discountParameter = parameterName(this.implementation.params[1])
+  }
+}
+
+class DaysInBudget extends PublicApi {
+  public get budgetParameter(): string {
+    return this.parameter
+  }
+
+  public readonly ratePerHourParameter: string
+  public readonly discountParameter: string
+
+  constructor(implementation: ExtractedFunction) {
+    super(implementation)
+
+    this.ratePerHourParameter = parameterName(this.implementation.params[1])
+    this.discountParameter = parameterName(this.implementation.params[2])
+  }
+}
+
+export class FreelancerRatesSolution {
+  private readonly source: Source
+
+  public readonly dayRate: DayRate
+  public readonly monthRate: MonthRate
+  public readonly daysInBudget: DaysInBudget
+
+  private exemplar!: Source
+
+  constructor(public readonly program: TSESTree.Program, source: string) {
+    this.source = new Source(source)
+
+    const functions = extractFunctions(program)
+    const exports = extractExports(program)
+
+    this.dayRate = new DayRate(assertPublicApi(DAY_RATE, exports, functions))
+
+    this.monthRate = new MonthRate(
+      assertPublicApi(MONTH_RATE, exports, functions)
+    )
+
+    this.daysInBudget = new DaysInBudget(
+      assertPublicApi(DAYS_IN_BUDGET, exports, functions)
+    )
+  }
+
+  public readExemplar(directory: string): void {
+    const configPath = path.join(directory, '.meta', 'config.json')
+    const config = JSON.parse(readFileSync(configPath).toString())
+
+    const exemplarPath = path.join(directory, config.files.exemplar[0])
+    this.exemplar = new Source(readFileSync(exemplarPath).toString())
+  }
+
+  public get isExemplar(): boolean {
+    const sourceAst = AstParser.REPRESENTER.parseSync(this.source.toString())
+    const exemplarAst = AstParser.REPRESENTER.parseSync(
+      this.exemplar.toString()
+    )
+
+    return (
+      JSON.stringify(sourceAst[0].program) ===
+      JSON.stringify(exemplarAst[0].program)
+    )
+  }
+}
--- src/analyzers/concept/freelancer-rates/index.ts
+++ src/analyzers/concept/freelancer-rates/index.ts
@@ -1,7 +1,63 @@
-import { ExemplarAnalyzer } from '../__exemplar'
+import {
+  AstParser,
+  Input,
+  NoExportError,
+  NoMethodError,
+} from '@exercism/static-analysis'
+import type { TSESTree } from '@typescript-eslint/typescript-estree'
+import {
+  EXEMPLAR_SOLUTION,
+  NO_METHOD,
+  NO_NAMED_EXPORT,
+} from '../../../comments/shared'
+import { ExecutionOptions, WritableOutput } from '../../../interface'
+import { IsolatedAnalyzerImpl } from '../../IsolatedAnalyzerImpl'
+import { FreelancerRatesSolution } from './FreelancerRatesSolution'
 
-export class FreelancerRatesAnalyzer extends ExemplarAnalyzer {
-  // TODO: implement actual analyzer
+type Program = TSESTree.Program
+
+export class FreelancerRatesAnalyzer extends IsolatedAnalyzerImpl {
+  private solution!: FreelancerRatesSolution
+
+  protected async execute(
+    input: Input,
+    output: WritableOutput,
+    options: ExecutionOptions
+  ): Promise<void> {
+    const [parsed] = await AstParser.ANALYZER.parse(input)
+
+    this.solution = this.checkStructure(parsed.program, parsed.source, output)
+    this.solution.readExemplar(options.inputDir)
+
+    if (this.solution.isExemplar) {
+      output.add(EXEMPLAR_SOLUTION())
+      output.finish()
+    }
+
+    output.finish()
+  }
+
+  private checkStructure(
+    program: Readonly<Program>,
+    source: Readonly<string>,
+    output: WritableOutput
+  ): FreelancerRatesSolution | never {
+    try {
+      return new FreelancerRatesSolution(program, source)
+    } catch (error) {
+      if (error instanceof NoMethodError) {
+        output.add(NO_METHOD({ 'method.name': error.method }))
+        output.finish()
+      }
+
+      if (error instanceof NoExportError) {
+        output.add(NO_NAMED_EXPORT({ 'export.name': error.namedExport }))
+        output.finish()
+      }
+
+      throw error
+    }
+  }
 }
 
 export default FreelancerRatesAnalyzer
