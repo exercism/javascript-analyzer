9c1dc1d360b7f864055b5ae668b7ef6f9a38bf3a
Rob Simpson <rsimpson2@me.com>
2021-12-27 15:24:09 -0500

    Add basic function not optimal check

--- src/analyzers/concept/vehicle-purchase/VehiclePurchaseSolution.ts
+++ src/analyzers/concept/vehicle-purchase/VehiclePurchaseSolution.ts
@@ -1,1 +1,169 @@
-export class VehiclePurchaseSolution {}
+import {
+  AstParser,
+  ExtractedFunction,
+  extractExports,
+  extractFunctions,
+} from '@exercism/static-analysis'
+import { AST_NODE_TYPES, TSESTree } from '@typescript-eslint/typescript-estree'
+import { readFileSync } from 'fs'
+import path from 'path'
+import { Source } from '~src/analyzers/SourceImpl'
+import { assertPublicApi } from '~src/asserts/assert_public_api'
+import { PublicApi } from '../../PublicApi'
+
+export const NEEDS_LICENSE = 'needsLicense'
+export const CHOOSE_VEHICLE = 'chooseVehicle'
+export const CALCULATE_RESELL_PRICE = 'calculateResellPrice'
+
+class NeedsLicense extends PublicApi {
+  constructor(public readonly implementation: ExtractedFunction) {
+    super(implementation)
+  }
+
+  public get isOptimal(): boolean {
+    if (this.implementation.params.length !== 1) {
+      return false
+    }
+
+    let foundSuboptimalNode = false
+
+    this.traverse({
+      enter() {
+        foundSuboptimalNode = true
+      },
+
+      [AST_NODE_TYPES.ReturnStatement]() {
+        foundSuboptimalNode = false
+      },
+
+      [AST_NODE_TYPES.BlockStatement]() {
+        foundSuboptimalNode = false
+      },
+
+      exit() {
+        if (foundSuboptimalNode) {
+          this.break()
+        }
+      },
+    })
+
+    return !foundSuboptimalNode
+  }
+}
+class ChooseVehicle extends PublicApi {
+  constructor(public readonly implementation: ExtractedFunction) {
+    super(implementation)
+  }
+
+  public get isOptimal(): boolean {
+    if (this.implementation.params.length !== 2) {
+      return false
+    }
+
+    let foundSuboptimalNode = false
+
+    this.traverse({
+      enter() {
+        foundSuboptimalNode = true
+      },
+
+      [AST_NODE_TYPES.ReturnStatement]() {
+        foundSuboptimalNode = false
+      },
+
+      [AST_NODE_TYPES.BlockStatement]() {
+        foundSuboptimalNode = false
+      },
+
+      exit() {
+        if (foundSuboptimalNode) {
+          this.break()
+        }
+      },
+    })
+
+    return !foundSuboptimalNode
+  }
+}
+class CalculateResellPrice extends PublicApi {
+  constructor(public readonly implementation: ExtractedFunction) {
+    super(implementation)
+  }
+
+  public get isOptimal(): boolean {
+    if (this.implementation.params.length !== 2) {
+      return false
+    }
+
+    let foundSuboptimalNode = false
+
+    this.traverse({
+      enter() {
+        foundSuboptimalNode = true
+      },
+
+      [AST_NODE_TYPES.ReturnStatement]() {
+        foundSuboptimalNode = false
+      },
+
+      [AST_NODE_TYPES.BlockStatement]() {
+        foundSuboptimalNode = false
+      },
+
+      exit() {
+        if (foundSuboptimalNode) {
+          this.break()
+        }
+      },
+    })
+
+    return !foundSuboptimalNode
+  }
+}
+
+export class VehiclePurchaseSolution {
+  private readonly source: Source
+
+  public readonly needsLicense: NeedsLicense
+  public readonly chooseVehicle: ChooseVehicle
+  public readonly calculateResellPrice: CalculateResellPrice
+
+  private exemplar!: Source
+
+  constructor(public readonly program: TSESTree.Program, source: string) {
+    this.source = new Source(source)
+
+    const functions = extractFunctions(program)
+    const exports = extractExports(program)
+
+    this.needsLicense = new NeedsLicense(
+      assertPublicApi(NEEDS_LICENSE, exports, functions)
+    )
+    this.chooseVehicle = new ChooseVehicle(
+      assertPublicApi(CHOOSE_VEHICLE, exports, functions)
+    )
+    this.calculateResellPrice = new CalculateResellPrice(
+      assertPublicApi(CALCULATE_RESELL_PRICE, exports, functions)
+    )
+  }
+
+  public readExemplar(directory: string): void {
+    const configPath = path.join(directory, '.meta', 'config.json')
+    const config = JSON.parse(readFileSync(configPath).toString())
+
+    const exemplarPath = path.join(directory, config.files.exemplar[0])
+    this.exemplar = new Source(readFileSync(exemplarPath).toString())
+  }
+
+  public get isExemplar(): boolean {
+    const sourceAst = AstParser.REPRESENTER.parseSync(this.source.toString())
+    const exemplarAst = AstParser.REPRESENTER.parseSync(
+      this.exemplar.toString()
+    )
+
+    return (
+      JSON.stringify(sourceAst[0].program) ===
+      JSON.stringify(exemplarAst[0].program)
+    )
+  }
+}
--- src/analyzers/concept/vehicle-purchase/index.ts
+++ src/analyzers/concept/vehicle-purchase/index.ts
@@ -1,2 +1,82 @@
+import {
+  AstParser,
+  Input,
+  NoExportError,
+  NoMethodError,
+} from '@exercism/static-analysis'
+import { TSESTree } from '@typescript-eslint/typescript-estree'
+import {
+  EXEMPLAR_SOLUTION,
+  FUNCTION_NOT_OPTIMAL,
+  NO_METHOD,
+  NO_NAMED_EXPORT,
+} from '../../../comments/shared'
+import { WritableOutput, ExecutionOptions } from '../../../interface'
 import { IsolatedAnalyzerImpl } from '../../IsolatedAnalyzerImpl'
-export class VehiclePurchaseAnalyzer extends IsolatedAnalyzerImpl {}
+import {
+  CALCULATE_RESELL_PRICE,
+  CHOOSE_VEHICLE,
+  NEEDS_LICENSE,
+  VehiclePurchaseSolution,
+} from './VehiclePurchaseSolution'
+type Program = TSESTree.Program
+
+export class VehiclePurchaseAnalyzer extends IsolatedAnalyzerImpl {
+  private solution!: VehiclePurchaseSolution
+
+  protected async execute(
+    input: Input,
+    output: WritableOutput,
+    options: ExecutionOptions
+  ): Promise<void> {
+    const [parsed] = await AstParser.ANALYZER.parse(input)
+
+    this.solution = this.checkStructure(parsed.program, parsed.source, output)
+    this.solution.readExemplar(options.inputDir)
+
+    if (this.solution.isExemplar) {
+      output.add(EXEMPLAR_SOLUTION())
+      output.finish()
+    }
+
+    if (!this.solution.needsLicense.isOptimal) {
+      output.add(FUNCTION_NOT_OPTIMAL({ function: NEEDS_LICENSE }))
+      output.finish()
+    }
+
+    if (!this.solution.chooseVehicle.isOptimal) {
+      output.add(FUNCTION_NOT_OPTIMAL({ function: CHOOSE_VEHICLE }))
+      output.finish()
+    }
+
+    if (!this.solution.calculateResellPrice.isOptimal) {
+      output.add(FUNCTION_NOT_OPTIMAL({ function: CALCULATE_RESELL_PRICE }))
+      output.finish()
+    }
+
+    output.finish()
+  }
+
+  private checkStructure(
+    program: Readonly<Program>,
+    source: Readonly<string>,
+    output: WritableOutput
+  ): VehiclePurchaseSolution | never {
+    try {
+      return new VehiclePurchaseSolution(program, source)
+    } catch (error: unknown) {
+      if (error instanceof NoMethodError) {
+        output.add(NO_METHOD({ 'method.name': error.method }))
+        output.finish()
+      }
+
+      if (error instanceof NoExportError) {
+        output.add(NO_NAMED_EXPORT({ 'export.name': error.namedExport }))
+      }
+
+      throw error
+    }
+  }
+}
+
+export default VehiclePurchaseAnalyzer
