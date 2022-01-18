4a7b973de43c11db02f6eb479cd8ae952ef3c507
Rob Simpson <rsimpson2@me.com>
2022-01-17 19:46:34 -0500

    Add Analyzer for Vehicle Purchase (#132)

---
+++ src/analyzers/concept/vehicle-purchase/VehiclePurchaseSolution.ts
@@ -0,0 +1,184 @@
+import {
+  AstParser,
+  ExtractedFunction,
+  extractExports,
+  extractFunctions,
+  findAll,
+  findFirst,
+  findFirstOfType,
+  TemplateLiteral,
+} from '@exercism/static-analysis'
+import { AST_NODE_TYPES, TSESTree } from '@typescript-eslint/typescript-estree'
+import { readFileSync } from 'fs'
+import path from 'path'
+import { Source } from '~src/analyzers/SourceImpl'
+import { assertPublicApi } from '~src/asserts/assert_public_api'
+import { PublicApi } from '../../PublicApi'
+
+type IfStatement = TSESTree.IfStatement
+
+export const NEEDS_LICENSE = 'needsLicense'
+export const CHOOSE_VEHICLE = 'chooseVehicle'
+export const CALCULATE_RESELL_PRICE = 'calculateResellPrice'
+
+/**
+ * Verify that `needsLicense` does not include an unnecessary if-statement where
+ * the student returns `true`/`false`.
+ */
+class NeedsLicense extends PublicApi {
+  constructor(public readonly implementation: ExtractedFunction) {
+    super(implementation)
+  }
+
+  public get hasConditional(): boolean {
+    return !!findFirstOfType(
+      this.implementation.body,
+      AST_NODE_TYPES.IfStatement
+    )
+  }
+}
+
+class ChooseVehicle extends PublicApi {
+  constructor(public readonly implementation: ExtractedFunction) {
+    super(implementation)
+  }
+
+  /**
+   *  Verify the student actually practiced if/else and did not use early returns.
+   */
+  public get usesIfElse(): boolean | undefined {
+    const body = this.implementation.body
+    // Only block statements can be inspected. Use `undefined` to signal
+    // the analyzer does not know if it uses if / else.
+    if (body.type !== AST_NODE_TYPES.BlockStatement) {
+      return undefined
+    }
+    const ifStatement = findFirst(
+      body,
+      (node): node is IfStatement => node.type === AST_NODE_TYPES.IfStatement
+    )
+    return Boolean(ifStatement?.alternate)
+  }
+
+  /**
+   * Verify that the template literal ` is clearly the better choice`
+   * only appears once.
+   */
+  public get isUsingSameTemplatedString(): boolean | undefined {
+    const templateValue = ` is clearly the better choice.`
+
+    // expression: "TemplateLiteral"
+    return (
+      // if there are multiple expressions we know the template literal appears
+      // more than once
+      findAll(
+        this.implementation.body,
+        (node): node is TSESTree.TemplateLiteral =>
+          // type: "TemplateLiteral"
+          node.type === AST_NODE_TYPES.TemplateLiteral
+      ).filter((expression: TSESTree.TemplateLiteral) =>
+        // quasis
+        expression.quasis
+          .map(
+            // value: {raw: string; cooked: string}
+            (templateElement: TSESTree.TemplateElement) =>
+              templateElement.value.raw
+          )
+          // check for some in case there are other template literals used
+          .some((value: string) => value.trim === templateValue.trim)
+      ).length > 1
+    )
+  }
+
+  /**
+   * Verify that the literal string ` is clearly the better choice`
+   * only appears once.
+   */
+  public get isUsingSameLiteralString(): boolean | undefined {
+    const templateValue = ' is clearly the better choice.'
+
+    // Literal
+    return (
+      findAll(
+        this.implementation.body,
+        (node): node is TSESTree.Literal =>
+          // type: "Literal"
+          node.type === AST_NODE_TYPES.Literal
+      ).filter(
+        (literal: TSESTree.Literal) =>
+          // {value: string} - should it be value or raw?
+          literal.value?.toString().trim === templateValue.trim
+      ).length > 1
+    )
+  }
+}
+
+class CalculateResellPrice extends PublicApi {
+  constructor(public readonly implementation: ExtractedFunction) {
+    super(implementation)
+  }
+
+  /**
+   * Verify the student actually practiced if/else and did not use early returns.
+   */
+  public get usesIfElse(): boolean | undefined {
+    const body = this.implementation.body
+    // Only block statements can be inspected. Use `undefined` to signal
+    // the analyzer does not know if it uses if / else.
+    if (body.type !== AST_NODE_TYPES.BlockStatement) {
+      return undefined
+    }
+    const ifStatement = findFirst(
+      body,
+      (node): node is IfStatement => node.type === AST_NODE_TYPES.IfStatement
+    )
+    return Boolean(ifStatement?.alternate)
+  }
+}
+
+export class VehiclePurchaseSolution {
+  private readonly source: Source
+
+  public readonly needsLicense: NeedsLicense
+  public readonly chooseVehicle: ChooseVehicle
+  public readonly calculateResellPrice: CalculateResellPrice
+
+  private exemplar!: Source
+
+  constructor(public readonly program: TSESTree.Program, source: string) {
+    this.source = new Source(source)
+
+    const functions = extractFunctions(program)
+    const exports = extractExports(program)
+
+    this.needsLicense = new NeedsLicense(
+      assertPublicApi(NEEDS_LICENSE, exports, functions)
+    )
+    this.chooseVehicle = new ChooseVehicle(
+      assertPublicApi(CHOOSE_VEHICLE, exports, functions)
+    )
+    this.calculateResellPrice = new CalculateResellPrice(
+      assertPublicApi(CALCULATE_RESELL_PRICE, exports, functions)
+    )
+  }
+
+  public readExemplar(directory: string): void {
+    const configPath = path.join(directory, '.meta', 'config.json')
+    const config = JSON.parse(readFileSync(configPath).toString())
+
+    const exemplarPath = path.join(directory, config.files.exemplar[0])
+    this.exemplar = new Source(readFileSync(exemplarPath).toString())
+  }
+
+  public get isExemplar(): boolean {
+    const sourceAst = AstParser.REPRESENTER.parseSync(this.source.toString())
+    const exemplarAst = AstParser.REPRESENTER.parseSync(
+      this.exemplar.toString()
+    )
+
+    return (
+      JSON.stringify(sourceAst[0].program) ===
+      JSON.stringify(exemplarAst[0].program)
+    )
+  }
+}
---
+++ src/analyzers/concept/vehicle-purchase/index.ts
@@ -0,0 +1,124 @@
+import {
+  AstParser,
+  Input,
+  NoExportError,
+  NoMethodError,
+} from '@exercism/static-analysis'
+import { TSESTree } from '@typescript-eslint/typescript-estree'
+import { CommentType, factory } from '../../../comments/comment'
+import {
+  EXEMPLAR_SOLUTION,
+  FUNCTION_NOT_OPTIMAL,
+  NO_METHOD,
+  NO_NAMED_EXPORT,
+} from '../../../comments/shared'
+import { WritableOutput, ExecutionOptions } from '../../../interface'
+import { IsolatedAnalyzerImpl } from '../../IsolatedAnalyzerImpl'
+import {
+  CALCULATE_RESELL_PRICE,
+  CHOOSE_VEHICLE,
+  NEEDS_LICENSE,
+  VehiclePurchaseSolution,
+} from './VehiclePurchaseSolution'
+type Program = TSESTree.Program
+
+const UNNECESSARY_IF_STATEMENT = factory`
+Instead of creating an \`if (...) {}\` block, leverage the boolean value directly
+that can be returned from checking the value of the parameter.
+
+For example, the following:
+
+\`\`\`javascript
+if (param === true) {
+  return true;
+}
+\`\`\`
+
+can be turned into:
+
+\`\`\`javascript
+return param === true
+\`\`\`
+`(
+  'javascript.vehicle-purchase.unnecessary_if_statement',
+  CommentType.Actionable
+)
+
+const USE_IF_ELSE = factory<'function'>`
+Nice.
+That's an \_early return\_ used in the \`${'function'}\` function.
+For the purpose of the Concept that this exercise aims to teach, try solving this
+using an \`else\` statement.
+`('javascript.vehicle-purchase.use_if_else', CommentType.Informative)
+
+const AVOID_DUPLICATE_LITERAL_USAGE = factory`
+Your code contains some unnecessary duplication.
+Think about a way to avoid that by first deciding for an option and then building up the final result.
+`(
+  'javascript.vehicle-purchase.avoid_duplicate_literal_usage',
+  CommentType.Informative
+)
+
+export class VehiclePurchaseAnalyzer extends IsolatedAnalyzerImpl {
+  private solution!: VehiclePurchaseSolution
+
+  protected async execute(
+    input: Input,
+    output: WritableOutput,
+    options: ExecutionOptions
+  ): Promise<void> {
+    const [parsed] = await AstParser.ANALYZER.parse(input)
+
+    this.solution = this.checkStructure(parsed.program, parsed.source, output)
+    this.solution.readExemplar(options.inputDir)
+
+    if (this.solution.isExemplar) {
+      output.add(EXEMPLAR_SOLUTION())
+      output.finish()
+    }
+
+    if (this.solution.needsLicense.hasConditional) {
+      output.add(UNNECESSARY_IF_STATEMENT())
+    }
+
+    if (!this.solution.chooseVehicle.usesIfElse) {
+      output.add(USE_IF_ELSE({ function: CHOOSE_VEHICLE }))
+    }
+
+    if (
+      this.solution.chooseVehicle.isUsingSameLiteralString ||
+      this.solution.chooseVehicle.isUsingSameTemplatedString
+    ) {
+      output.add(AVOID_DUPLICATE_LITERAL_USAGE())
+    }
+
+    if (!this.solution.calculateResellPrice.usesIfElse) {
+      output.add(USE_IF_ELSE({ function: CALCULATE_RESELL_PRICE }))
+    }
+
+    output.finish()
+  }
+
+  private checkStructure(
+    program: Readonly<Program>,
+    source: Readonly<string>,
+    output: WritableOutput
+  ): VehiclePurchaseSolution | never {
+    try {
+      return new VehiclePurchaseSolution(program, source)
+    } catch (error: unknown) {
+      if (error instanceof NoMethodError) {
+        output.add(NO_METHOD({ 'method.name': error.method }))
+        output.finish()
+      }
+
+      if (error instanceof NoExportError) {
+        output.add(NO_NAMED_EXPORT({ 'export.name': error.namedExport }))
+      }
+
+      throw error
+    }
+  }
+}
+
+export default VehiclePurchaseAnalyzer
